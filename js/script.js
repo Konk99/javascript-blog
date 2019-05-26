dbae6d8d0070358169e1a2b54f9b717a8fca305f
Konrad Kostrzanowski <konk99@o2.pl>
2019-05-26 19:05:55 +0200

    Add function remove class 'active'

--- js/script.js
+++ js/script.js
@@ -4,8 +4,18 @@ function titleClickHandler() {
     console.log('Link was clicked!');
     console.log(event)
     /* remove class 'active' from all acrticle links */
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for (let activeLink of activeLinks) {
+        activeLink.classList.remove('active');
+    }
 
     /* add class 'active' to the clicked link */
+    const activeArticles = document.querySelectorAll('.posts article.active');
+
+    for (let activeArticle of activeArticles) {
+        activeArticle.classList.remove('active');
+    }
 
     /* remove class 'active' from all articles */
 
